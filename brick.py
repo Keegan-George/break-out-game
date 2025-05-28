c881828269333364861b2f0d35326add6921bb2c
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-05-28 11:52:04 -0400

    Add Brick class to config. Update Brick references accordingly. Modify Brick remove function to also hide the turtle

--- brick.py
+++ brick.py
@@ -1,16 +1,15 @@
-from config import *
 from turtle import Turtle
+from config import BrickConfig
 
 
 class Brick(Turtle):
     def __init__(self) -> None:
         super().__init__()
-        self.shape("square")
-        self.shapesize(stretch_len=BRICK_STRETCH_LENGTH)
+        self.shape(BrickConfig.SHAPE)
+        self.shapesize(stretch_len=BrickConfig.STRETCH_LENGTH)
         self.penup()
 
     def remove(self):
-        self.reset()
-
-        # since turtle objects cannot be deleted move it off screen
+        self.clear()
+        self.hideturtle()
         self.goto(10000, 10000)
