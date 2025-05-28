51f00ee30527af152304d9ff33496bd91e637c83
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-05-28 14:11:37 -0400

    Update main class references

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from config import *
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 from time import sleep
 from brick_factory import BrickFactory
 from scoreboard import ScoreBoard
+from config import *
 
 screen = Screen()
-screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
-screen.bgcolor(SCREEN_BACKGROUND_COLOUR)
-screen.title("Break Out Game")
+screen.setup(width=ScreenConfig.WIDTH, height=ScreenConfig.HEIGHT)
+screen.bgcolor(ScreenConfig.BACKGROUND_COLOUR)
+screen.title(ScreenConfig.TITLE)
 screen.tracer(0)
 
 paddle = Paddle()
 ball = Ball()
 scoreboard = ScoreBoard()
 brick_factory = BrickFactory()
-brick_factory.generate_bricks()
+brick_factory.generate_brick_wall()
 
 screen.listen()
 screen.onkeypress(paddle.move_left, "Left")
@@ -28,24 +28,24 @@ while scoreboard.lives and brick_factory.bricks:
 
     ball.move()
 
-    if ball.has_gone_below_screen():
+    if ball.is_below_screen():
         scoreboard.lives -= 1
         scoreboard.update_score()
         ball.bounce_y()
         ball.goto(paddle.xcor(), paddle.ycor() + PADDLE_WIDTH)
 
-    if ball.has_collided_with_upper_wall():
+    if ball.has_hit_upper_wall():
         ball.bounce_y()
 
-    if ball.has_collided_with_side_wall():
+    if ball.has_hit_side_wall():
         ball.bounce_x()
 
-    if ball.has_collided_with_other_object(paddle):
+    if ball.has_hit_other_object(paddle):
         ball.bounce_y()
 
     for i in range(len(brick_factory.bricks) - 1, -1, -1):
         brick = brick_factory.bricks[i]
-        if ball.has_collided_with_other_object(brick):
+        if ball.has_hit_other_object(brick):
             scoreboard.score += 1
             scoreboard.update_score()
             ball.bounce_y()
